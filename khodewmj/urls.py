debe1a386087972023cf3221e7f50ff59bbd235d
aiza <aizamoghaddam@gmail.com>
2024-06-11 19:43:47 +0330

    update backend

---
+++ khodewmj/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls', namespace='home') ),
+    path('examples/', include('examples.urls', namespace='examples')),
+    path('resume/', include('resume.urls', namespace='resume')),
+    path('skills/', include('skills.urls', namespace='skills'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
