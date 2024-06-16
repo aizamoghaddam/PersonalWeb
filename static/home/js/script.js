ff74688d4e0bf471e8f5059ab244e6620a5e7f71
M M D <153559543+khodewmj@users.noreply.github.com>
2024-06-16 18:24:53 +0330

    update files and us js

--- static/home/js/script.js
+++ static/home/js/script.js
@@ -1,2 +1,51 @@
 let $ = document
 
+// Modal btn Condition
+const modal = $.getElementById('ModalConection')
+let btnShowModal = $.getElementById('ClickMeBtn')
+let exitModalElem = $.getElementById('Exit-Modal')
+
+// Download File PDF Resume
+function downloadFile() {
+
+  const fileResume = $.createElement('a')
+  
+  fileResume.setAttribute('href', '../DownloadsFile/ComingSoon.txt')
+  
+  fileResume.setAttribute('download', 'ComingSoon.txt')
+
+  $.body.appendChild(fileResume)
+  fileResume.click()
+
+  $.body.removeChild(fileResume)
+}
+// WorkSample Btn Go GItHUb
+function workSampleLink() {
+  window.location.href = 'https://github.com/khodewmj'
+}
+
+// Modal btn Condition
+function showModal() {
+  modal.style.display = 'block'
+  modal.style.backdropFilter = 'blur(12px)'
+}
+function exitModal() {
+  modal.style.display = 'none'
+}
+function hideModaWithEsc(event) {
+  if (event.keyCode === 27) {
+      modal.style.display = 'none'
+  }
+}
+
+
+// Download File PDF Resume
+$.getElementById('DownloadsFile').addEventListener('click', downloadFile)
+
+// WorkSample Btn Go GItHUb
+$.getElementById('WorkSample').addEventListener('click', workSampleLink)
+
+// Modal btn Condition
+btnShowModal.addEventListener('click', showModal)
+exitModalElem.addEventListener('click', exitModal)
+$.body.addEventListener('keyup', hideModaWithEsc)
